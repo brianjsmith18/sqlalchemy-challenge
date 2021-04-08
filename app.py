f9076756fccc07201c2afcf9448ab06bf89c9828
Brian <brianjsmith18@gmail.com>
2021-04-08 17:30:07 -0400

    app py

---
+++ app.py
@@ -0,0 +1,79 @@
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+session = Session(engine)
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def prcp():
+    # Create our session (link) from Python to the DB
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= query_date).all()
+
+    # Convert list of tuples into normal list
+    # all_names = list(np.ravel(results))
+    p_list = []
+    for x , y in results:
+        p_dict = {}
+        p_dict ["date"]= x
+        p_dict ["prcp"]= y
+        p_list.append(p_dict)
+    return jsonify(p_list)
+
+
+@app.route("/api/v1.0/passengers")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
